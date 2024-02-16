ad69516a884696e94822620ebd53d29cf1e6e35f
Caden Huffman <cadenhuf@bu.edu>
2024-02-16 12:11:22 +0000

    Got Dir function working, moving on to environ

--- simpleshell.c
+++ simpleshell.c
@@ -8,8 +8,17 @@ int clr(void){
 	return 0;	
 }
 
-int dir(void){
-	system("ls -al");
+int dir(char directory[]){
+	
+	if(strlen(directory) > 0){
+		char command[100];
+                sprintf(command, "ls -al %s", directory);
+                system(command);
+	
+	}else{
+		system("ls -al");
+	}
+	
 	return 0;
 }
 
@@ -29,22 +38,30 @@ int main(int argc, char *argv[]){
 	char * prompt = "==>";
 	
 
-	//Command 
-	char str[100];
+	//Command max 100 chars
+	char input[200]; 
+	char command[100];
+	char directory[100];
 
 	//Writes prompt to stdout
 	fputs(prompt, stdout);
 
-	//Reads prompt from stdin and stores in str
-	fgets(str, sizeof(str), stdin);
+	//Read one word command from stdin and stores in str
 	
-	if (strcmp("clear",str) != 0){
+	fgets(input, sizeof(input), stdin);
+
+  	// Parse input to extract command and directory
+    	sscanf(input, "%s %s", command, directory);
+
+
+	if (!strcmp("clear",command)){
 		clr();	
 	}
 
-	if (strcmp("dir", str) != 0){
-		//Need to somehow get the argument 
-		dir();
+	if (!strcmp("dir", command)){
+		//Need to somehow get the argument
+		 
+		dir(directory);
 	}
 
 	
